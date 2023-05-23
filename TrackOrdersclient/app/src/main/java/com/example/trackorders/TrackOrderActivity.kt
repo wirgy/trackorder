50608aa6da3bb59d5bb89102a8bc58a3e1bb817c
wirgy <tima.homyakov26072002@gmail.com>
2023-05-23 15:04:17 +0800

    создание формы для отслеживания заказа

---
+++ TrackOrdersclient/app/src/main/java/com/example/trackorders/TrackOrderActivity.kt
@@ -0,0 +1,11 @@
+package com.example.trackorders
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrackOrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_track_order)
+    }
+}
